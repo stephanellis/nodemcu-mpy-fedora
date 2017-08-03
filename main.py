02581bee02d4cfa60cb7f4c7e4b122452f71747e
Stephan M Ellis <stephan@ellisopen.com>
2017-08-02 23:34:44 -0500

    added a settings json file and updated the put script to install it on the nodemcu

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ DELAY=1
 p = machine.Pin(2, machine.Pin.OUT)
 
 while True:
+    print("LED off...")
     p.on()
     time.sleep(DELAY)
+    print("LED on...")
     p.off()
     time.sleep(DELAY)
     p.on()
---
+++ put_main.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+ampy -p /dev/ttyUSB0 put main.py
+ampy -p /dev/ttyUSB0 put settings.json
