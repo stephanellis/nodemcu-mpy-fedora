9b05dcef526b6cd04ea837cb456d7a5eeb4379ef
Stephan M Ellis <stephan@ellisopen.com>
2017-08-09 23:53:57 -0500

    added support for my new dht11, works great

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 
+import esp
+import dht
 import time
 import json
 import machine
@@ -10,9 +12,10 @@ p = machine.Pin(2, machine.Pin.OUT)
 p.on()
 p2 = machine.Pin(4, machine.Pin.PULL_UP)
 
+d = dht.DHT11(machine.Pin(2))
 
 BLINK_DELAY=0.1
-MEASR_DELAY=10
+MEASR_DELAY=2
 
 def blink():
     p.off()
@@ -22,7 +25,16 @@ def blink():
     time.sleep(BLINK_DELAY)
     p2.off()
 
-    
+
+def get_rssi():
+    nets = sta.scan()
+    for n in nets:
+        if n[0].decode() == settings["network"]:
+            return n[3]
+    return None
+
+def ctof(t):
+    return t * (9 / 5) + 32
 
 f = open("settings.json")
 raw_settings = "".join(f.readlines())
@@ -42,7 +54,10 @@ while True:
         break
     time.sleep(1)
 
+counter = 0
 while True:
+    d.measure()
     blink()
-    c.publish(settings["topic"], "Blinked!")
+    c.publish(settings["topic"], "Blinked! Free mem: {}, Count: {}, Temp: {}, RH: {}, RSSI: {}".format(esp.freemem(), counter, ctof(d.temperature()), d.humidity(), get_rssi()))
+    counter += 1
     time.sleep(MEASR_DELAY)
