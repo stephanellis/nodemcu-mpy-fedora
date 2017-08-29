2c04de0753d5526e363c7cc61755c59565ee7206
Stephan M Ellis <stephan@ellisopen.com>
2017-08-28 22:43:00 -0500

    did some more experimentation with the ssd1306 oled display

--- main_ssd1306.py
+++ main_ssd1306.py
@@ -1,8 +1,19 @@
 import machine
 import ssd1306
 
+# make the pins D5 and D5 into I2C bus
 i2c = machine.I2C(scl=machine.Pin(14), sda=machine.Pin(12))
+# setup the ssd1306 display on that bus
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
-oled.text("esp8266, it's verkink!", 0,0)
+# you can fit 6 Lines of text
+# each with 16 characters
+# the yellow area is 16 pixels tall
+# the rest is in the blue area
+oled.text("esp8266 number 2", 0,0)
+oled.text(" It's verkink!--", 0, 16)
+oled.text(">Sensor Output--", 0, 26)
+oled.text(" Relay Control--", 0, 36)
+oled.text(" Relay Control--", 0, 46)
+oled.text(" Relay Control--", 0, 56)
 oled.show()
